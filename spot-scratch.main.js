953eb082e7887da5f2d92c0b9c17064c18d36d25
Shark Mobile Workstation <owensilva1282@gmail.com>
2022-03-02 17:48:54 -0500

    Initial Commit

---
+++ spot-scratch.main.js
@@ -0,0 +1,44 @@
+class SpotScratch {
+    constructor() {
+    }
+    
+    getInfo() {
+        return {
+            "id": "SpotScratch",
+            "name": "Spot! Scratch",
+            "blocks": [
+                {
+                    "opcode": "bodyCommand",
+                    "blockType": "command",
+                    "text": "send a [cmdName] command to spot",
+                    "arguments": {
+                        "cmdName": {
+                            "type": "string",
+                            "defaultValue": "sit",
+                            "menu": "cmdMenu"
+                        },
+                    }
+                }
+            ],
+            "menus": {
+                "cmdMenu": [{text:"sit",value:"sit"}, {text:"stand",value:"stand"}]
+            }       
+        };
+    }
+
+    bodyCommand({cmdName}) {
+        fetch('http://192.168.80.101:8000/command', {
+            method: 'POST',
+            headers: {},
+            body: JSON.stringify({
+              Command: cmdName // eg 'sit'
+            }),
+        });
+
+        return true;
+    }
+}
+
+Scratch.extensions.register(new SpotScratch())
+
+// data:text/javascript;base64,
